4a3a5bc4b13beaeb56614b088e3d084f01a50041
knoronha <kenneth.mario@gmail.com>
2017-03-29 10:10:25 -0500

    add second func

--- first.py
+++ first.py
@@ -1,1 +1,33 @@
-print "first"
+print " We are now going to implement some fiunctions "
+
+
+def Complement(inBitStr="0"):
+    outBitStr = ""
+    for bit in inBitStr:
+        if bit == "0":
+            outBitStr += "1"
+        else:
+            outBitStr += "0"
+    return outBitStr
+
+
+print Complement("1010101111")
+
+
+def Qencoder(value=0.0, inBits=4, fracbits=2, signed=1, roundMethod="ROUND"):
+    if roundMethod == "ROUND":
+        roundok = True
+    if roundMethod == "CEIL":
+        roundok = True
+    if roundMethod == "FLOOR":
+        roundok = True
+    if roundok == False:
+        raise ValueError("Error: Value of roundmethod should be either of ROUND CEIL FLOOR")
+        return "error roundmethod"
+
+    if signed == 1:
+        isSigned = True
+    else:
+        isSigned = False
+
+ abs()
